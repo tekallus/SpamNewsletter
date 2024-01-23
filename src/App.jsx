2500ad3ff40a144ef09f4caef152234ad82def89
“tekallus <dorukkuheylan@gmail.com>
2024-01-23 18:02:46 +0300

    yeni posta adresi email liste ekleme eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,10 @@ export default function App() {
 
     // Yinelenen durumu state'e ayarla
     setDuplicate(isDuplicate)
+    // Yeni e-posta adresini emailList dizisine ekle
+    if (!isDuplicate) {
+      emailList.push(userInput.toLowerCase())
+    }
   }
 
   return (
